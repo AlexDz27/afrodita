9088124987af1e891bae615f85f6ae1c443e098e
Alex Dzyuba <alexeydzyuba27@gmail.com>
2021-03-09 00:05:33 +0300

    Refactor: margin-bottom for heading on admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,9 +5,10 @@
 @endsection
 
 @section('content')
-    <h1 class="mb-4">
+    <h1 style="margin-bottom: 250px;">
         Welcome to dashboard, Admin! Here you can edit products.<br>
-        Or go to <a href="{{ url('/admin/orders?time=today') }}">Orders page</a>
+        Or go to <a href="{{ url('/admin/bookings?time=today') }}">Bookings page</a><br>
+        Or go to <a href="{{ url('/admin/orders?time=today') }}">Orders page</a><br>
     </h1>
 
     @foreach ($products as $product)
